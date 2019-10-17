40deda937559bb47a2d35321f47653b2eb4f6542
Danny Calise <dannyjcalise@gmail.com>
2019-10-17 19:28:15 -0400

    finished do-what-it-says

--- liri.js
+++ liri.js
@@ -16,66 +16,59 @@ var getMovieInfo = function (qURL) {
         console.log(movieInfo);
     });
 }
-
-switch (command) {
-    case "concert-this":
-        var queryURL = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
-        axios.get(queryURL).then(function (response) {
-            for (let i = 0; i < (response.data).length; i++) {
-                console.log(response.data[i].venue.name, response.data[i].venue.city, response.data[i].venue.country, moment(response.data[i].datetime).format("L"));
-            }
-        });
-        break;
-    case "spotify-this-song":
-        var Spotify = require('node-spotify-api');
-        var spotify = new Spotify({
-            id: "f72cf46ec989400687f58b4144b65c1f",
-            secret: "d65e2ae116a84577ac0b4f4f1cd1be79"
-        });
-        var songInfo = [];
-        spotify
-            .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-            .then(function (data) {
-                songInfo.push(data.artists[0].name, data.name, data.external_urls.spotify, data.album.name);
-                console.log(songInfo);
-            })
-            .catch(function (err) {
-                console.error('Error occurred: ' + err);
-            });
-        break;
-    case "movie-this":
-        if (input === undefined) {
-            var queryURL = "http://www.omdbapi.com/?t=mr+nobody&apikey=a58d70c6";
-            getMovieInfo(queryURL);
-        } else {
-            var queryURL = "http://www.omdbapi.com/?t=" + input + "&apikey=a58d70c6";
-            getMovieInfo(queryURL);
-        }
-        break;
-    case "do-what-it-says":
-        var cmd = require('node-cmd');
-
-        fs.readFile('random.txt', 'utf8', function(err, text) {
-            cmd.get(
-                `node liri.js ` + text,
-                function(err, data, stderr){
-                    if (!err) {
-                       console.log(data)
-                    } else {
-                       console.log('error', err)
-                    }
-         
+function all(cmd, inpt) {
+    switch (cmd) {
+        case "concert-this":
+            var queryURL = "https://rest.bandsintown.com/artists/" + inpt + "/events?app_id=codingbootcamp";
+            axios.get(queryURL).then(function (response) {
+                for (let i = 0; i < (response.data).length; i++) {
+                    console.log(response.data[i].venue.name, response.data[i].venue.city, response.data[i].venue.country, moment(response.data[i].datetime).format("L"));
                 }
-            );
-        });
-        
+            });
+            break;
+        case "spotify-this-song":
+            function spot() {
+                var Spotify = require('node-spotify-api');
+                var spotify = new Spotify({
+                    id: "f72cf46ec989400687f58b4144b65c1f",
+                    secret: "d65e2ae116a84577ac0b4f4f1cd1be79"
+                });
+                var songInfo = [];
+                spotify
+                    .search({ type: 'track', query: inpt })
+                    .then(function (response) {
+                        // console.log(response.tracks);
+                        songInfo.push(response.tracks.items[0].artists[0].name, response.tracks.items[0].name, response.tracks.items[0].href, response.tracks.items[0].album.name);
+                        console.log(songInfo);
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    });
+            }
+            spot();
+            break;
+        case "movie-this":
+            if (input === undefined) {
+                var queryURL = "http://www.omdbapi.com/?t=mr+nobody&apikey=a58d70c6";
+                getMovieInfo(queryURL);
+            } else {
+                var queryURL = "http://www.omdbapi.com/?t=" + input + "&apikey=a58d70c6";
+                getMovieInfo(queryURL);
+            }
+            break;
+        case "do-what-it-says":
+            fs.readFile('random.txt', 'utf8', function (err, data) {
+                if (err) {
+                    console.log(err);
+                }
+                let split = data.split(',');
+                command = split[0];
+                input = split[1];
+                all(command, input);
+            });
+        // console.log(command, input);
+        // all(command, input);
+    }
 }
-
-// fs.readFile('random.txt', 'utf8', function (err, data) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//     }
-
-// });
+all(command, input);
 
